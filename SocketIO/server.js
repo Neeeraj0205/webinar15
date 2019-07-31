7b6538dc898bd2a45493f3dbc37c1f6104d90966
neeraj <neeeraj2599@gmail.com>
2019-07-31 20:49:47 +0530

    chatbot

---
+++ SocketIO/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const socketio = require('socket.io')
+const http = require('http')
+
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
+
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket) => {
+    console.log("New socket formed from " + socket.id)
+    socket.emit('connected')
+
+  
+    
+    socket.on('send_msg', (data) => {
+       io.emit('recv_msg',data)
+
+})
+})
+
+server.listen(2345, () => console.log('Website open on http://localhost:2345'))
